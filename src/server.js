1e569390d473d1a5496679e321c049cb69678177
namducbui <buiducnam2002@gmail.com>
2026-05-21 09:48:40 +0700

    Connect Prisma 5 vs PostgreSQL

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 const express = require('express')
+const prisma = require('./lib/prisma')
+
 const app = express()
-
 require ('dotenv').config()
-
 app.use(express.json())
 
 app.get('/', (req, res) => {
     res.json({message: 'Server đang chạy'})
+})
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await prisma.users.findMany()
+    res.json(users)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
 })
 
 const PORT = process.env.PORT || 3000
